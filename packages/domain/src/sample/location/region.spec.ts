ca08d1d111efe2c10fd06354f0d95fea03a8fb2d
Thiery Michel <thiery@marmelab.com>
2026-09-14 07:19:44 +0000

    refactor: drop the region tree test that compares a constant to itself

--- packages/domain/src/sample/location/region.spec.ts
+++ packages/domain/src/sample/location/region.spec.ts
@@ -5,11 +5,6 @@ import { OCEAN_SEAS } from "./ocean-sea.ts";
 import { REGION_TREE } from "./region.ts";
 
 describe("REGION_TREE", () => {
-  it("should offer every country under continent and every ocean or sea under ocean", () => {
-    expect(REGION_TREE.continent.choices).toEqual(COUNTRIES);
-    expect(REGION_TREE.ocean.choices).toEqual(OCEAN_SEAS);
-  });
-
   it("should give no leaf a segment that collides with a root", () => {
     const roots = Object.keys(REGION_TREE);
     expect(
